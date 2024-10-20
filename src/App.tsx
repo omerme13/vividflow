31ae5edfcbddf4b8a1ce65d5c405300243441068
omer <omerme13@gmail.com>
2024-10-20 03:49:32 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+import "./App.scss";
+import Card from "./components/Card/Card";
+
+const App: FC = () => {
+    return (
+        <div className="app">
+            <Card text="Find Nemo" />
+        </div>
+    );
+};
+
+export default App;
