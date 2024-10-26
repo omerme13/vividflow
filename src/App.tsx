74220098105f2a47fd1354937810335c438e63f2
omer <omerme13@gmail.com>
2024-10-26 18:35:36 +0300

    fix: add task provide to main file

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,16 @@ import { useState } from "react";
 import Sidebar from "@/components/Sidebar";
 import TasksList from "@/components/TasksList";
 import NewTaskModal from "./components/NewTaskModal/NewTaskModal";
-import { TaskProvider } from "./context/TaskContext";
 
 import "./App.scss";
 
 export default function App() {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-    const handleSubmit = () => {};
     return (
         <div className="app">
-            <TaskProvider>
-                <Sidebar openNewTaskModal={() => setIsModalOpen(true)} />
-                <TasksList />
-                <NewTaskModal
-                    isOpen={isModalOpen}
-                    onClose={() => setIsModalOpen(false)}
-                    onSubmit={handleSubmit}
-                    existingLabels={[]}
-                />
-            </TaskProvider>
+            <Sidebar openNewTaskModal={() => setIsModalOpen(true)} />
+            <TasksList />
+            <NewTaskModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} existingLabels={[]} />
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-import './styles/global.scss';
+import "./styles/global.scss";
+import { TaskProvider } from "./context/TaskContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <TaskProvider>
+            <App />
+        </TaskProvider>
+    </StrictMode>
+);
