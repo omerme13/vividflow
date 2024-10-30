88565beedf0345dc0b6acc949bf7f132d3f1eb83
omer <omerme13@gmail.com>
2024-10-30 19:30:29 +0200

    feat(layout toggle button): move jsx from task header and created a component

--- src/components/TasksHeader/TasksHeader.tsx
+++ src/components/TasksHeader/TasksHeader.tsx
@@ -1,10 +1,9 @@
-import { GridIcon, KanbanIcon, SearchIcon } from "@/assets/icons";
-import { useLayout } from "@/context/LayoutContext";
+import {  SearchIcon } from "@/assets/icons";
 
 import "./TasksHeader.scss";
+import LayoutToggleButton from "./LayoutToggleButton";
 
 export default function TasksHeader() {
-    const { layout, toggleViewMode } = useLayout();
 
     return (
         <div className="tasks-header">
@@ -12,21 +11,7 @@ export default function TasksHeader() {
                 <SearchIcon className="tasks-header__search-button" />
                 <input type="text" className="tasks-header__search-input" placeholder="Search" />
             </div>
-            <button className="tasks-header__layout-toggle-button">
-                <KanbanIcon
-                    className={`tasks-header__layout-toggle-button-icon ${
-                        layout.isGridViewMode ? "tasks-header__layout-toggle-button-icon--not-selected" : ""
-                    }`}
-                    onClick={toggleViewMode}
-                />
-                <div className="tasks-header__layout-toggle-button-separator" />
-                <GridIcon
-                    className={`tasks-header__layout-toggle-button-icon ${
-                        layout.isGridViewMode ? "" : "tasks-header__layout-toggle-button-icon--not-selected"
-                    }`}
-                    onClick={toggleViewMode}
-                />
-            </button>
+			<LayoutToggleButton />
         </div>
     );
 }
