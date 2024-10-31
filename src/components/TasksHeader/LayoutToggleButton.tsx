2f14ecfc852683b7a9d0a8b3937dd256f9803f29
omer <omerme13@gmail.com>
2024-10-31 20:15:54 +0200

    Change button order, add global focus state

--- src/components/TasksHeader/LayoutToggleButton.tsx
+++ src/components/TasksHeader/LayoutToggleButton.tsx
@@ -2,20 +2,20 @@ import { GridIcon, KanbanIcon } from "@/assets/icons";
 import { useLayout } from "@/context/LayoutContext";
 import { getClassWithModifier } from "@/utils/styles";
 
-import './LayoutToggleButton.scss';
+import "./LayoutToggleButton.scss";
 
 export default function LayoutToggleButton() {
     const { layout, toggleViewMode } = useLayout();
-	
+
     return (
         <button className="layout-toggle-button">
-            <KanbanIcon
-                className={getClassWithModifier("layout-toggle-button__icon", "disabled", layout.isGridViewMode)}
+            <GridIcon
+                className={getClassWithModifier("layout-toggle-button__icon", "disabled", !layout.isGridViewMode)}
                 onClick={toggleViewMode}
             />
             <div className="layout-toggle-button__separator" />
-            <GridIcon
-                className={getClassWithModifier("layout-toggle-button__icon", "disabled", !layout.isGridViewMode)}
+            <KanbanIcon
+                className={getClassWithModifier("layout-toggle-button__icon", "disabled", layout.isGridViewMode)}
                 onClick={toggleViewMode}
             />
         </button>
