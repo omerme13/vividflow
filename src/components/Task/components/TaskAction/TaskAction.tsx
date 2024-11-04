6ca86d1b46baee3cb6f5fe30791f93bbfbb76479
omer <omerme13@gmail.com>
2024-11-04 04:29:44 +0200

    feat: add active state to task action

--- src/components/Task/components/TaskAction/TaskAction.tsx
+++ src/components/Task/components/TaskAction/TaskAction.tsx
@@ -2,12 +2,14 @@ import { TaskActionProps } from "./TaskAction.types";
 import Tooltip from "@/components/Tooltip";
 
 import "./TaskAction.scss";
+import { getClassWithModifier } from "@/utils/styles";
 
-export default function TaskAction({ action, size, icon: Icon, isWarning, tooltipContent }: TaskActionProps) {
+export default function TaskAction({ action, size, icon: Icon, isWarning, tooltipContent, isActive }: TaskActionProps) {
     return (
         <Tooltip content={tooltipContent}>
             <Icon
-                className={`task-action ${isWarning ? "task-action--warning" : ""}`}
+                className={`${getClassWithModifier("task-action", "warning", !!isWarning)}
+				${getClassWithModifier("task-action", "active", isActive)}`}
                 onClick={action}
                 {...(size && { width: size, height: size })}
             />
