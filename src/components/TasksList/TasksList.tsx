f684d33e21ec2fe59ce9f3295762140ef5258b5d
omer <omerme13@gmail.com>
2024-11-04 04:33:02 +0200

    feat(tasks list): separate between done/undone tasks

--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -8,10 +8,10 @@ import { useLayout } from "@/context/LayoutContext";
 import "./TasksList.scss";
 
 export default function TaskList() {
-	const filteredTasks = useFilteredTasks();
+    const filteredTasks = useFilteredTasks();
     const [selectedTask, setSelectedTask] = useState<TaskData | null>(null);
     const { layout, toggleTaskModal } = useLayout();
-	
+
     const handleEdit = (task: TaskData) => {
         setSelectedTask(task);
         toggleTaskModal();
@@ -24,15 +24,22 @@ export default function TaskList() {
 
     return (
         <div className="tasks-list">
-            {filteredTasks.map((task) => (
-                <Task key={task.id} task={task} onEdit={() => handleEdit(task)} />
-            ))}
-            <TaskModal
-                isOpen={layout.isTaskModalOpen}
-                onClose={handleClose}
-                isEditMode={!!selectedTask}
-                task={selectedTask || undefined}
-            />
+            <div className="tasks-list__undone">
+                {filteredTasks.incomplete.map((task) => (
+                    <Task key={task.id} task={task} onEdit={() => handleEdit(task)} />
+                ))}
+                <TaskModal
+                    isOpen={layout.isTaskModalOpen}
+                    onClose={handleClose}
+                    isEditMode={!!selectedTask}
+                    task={selectedTask || undefined}
+                />
+            </div>
+            <div className="tasks-list__done">
+                {filteredTasks.completed.map((task) => (
+                    <Task key={task.id} task={task} onEdit={() => handleEdit(task)} />
+                ))}
+            </div>
         </div>
     );
 }
