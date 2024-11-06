b4002acce68f2c8f7fc040ebb90556548f4de48b
omer <omerme13@gmail.com>
2024-11-07 01:10:04 +0200

    prettify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
-import './App.scss';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
+import "./App.scss";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
