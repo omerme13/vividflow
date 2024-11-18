f739c45f078cb32c681c102ac5729608dcc17dfb
omer <omerme13@gmail.com>
2024-11-18 19:57:37 +0200

    fix(color picker): fixed TS issues

--- src/components/ColorPicker/ColorPicker.types.ts
+++ src/components/ColorPicker/ColorPicker.types.ts
@@ -9,12 +9,16 @@ interface SingleColorProps extends BaseColorPickerProps {
     isMulti: false;
     color: TaskColors | undefined;
     onChangeColor: (color: TaskColors | undefined) => void;
+	colors: never;
+	onChangeColors: never;
 }
 
 interface MultipleColorsProps extends BaseColorPickerProps {
-    isMulti: true;
+	isMulti: true;
     colors: TaskColors[];
     onChangeColors: (colors: TaskColors[]) => void;
+	color: never;
+	onChangeColor: never;
 }
 
 export type ColorPickerProps = SingleColorProps | MultipleColorsProps;
