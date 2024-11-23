a2af437c5dd1b93e5b8391a2d6d845cb14f2e252
omer <omerme13@gmail.com>
2024-11-23 20:36:49 +0200

    style: dashboard item small fix

--- src/components/Dashboard/components/DashboardItem/DashboardItem.tsx
+++ src/components/Dashboard/components/DashboardItem/DashboardItem.tsx
@@ -2,18 +2,10 @@ import { getClassWithModifier } from "@/utils/styles";
 import "./DashboardItem.scss";
 import { DashboardItemProps } from "./DashboardItem.types";
 
-export default function DashboardItem({
-    title,
-    fullRow,
-    hasContainer,
-    children,
-    className,
-}: DashboardItemProps) {
+export default function DashboardItem({ title, fullRow, hasContainer, children, className }: DashboardItemProps) {
     return (
         <div className={`${className} ${getClassWithModifier("dashboard-item", "full", !!fullRow)}`}>
-            <div className="dashboard-item__header">
-                <h2 className="dashboard-item__title">{title}</h2>
-            </div>
+            <h2 className="dashboard-item__title">{title}</h2>
             {hasContainer ? <div className="dashboard-item__container">{children}</div> : children}
         </div>
     );
