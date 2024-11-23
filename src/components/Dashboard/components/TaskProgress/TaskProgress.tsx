6a10112c3267becc034c9154b68c6ac643226386
omer <omerme13@gmail.com>
2024-11-23 20:47:43 +0200

    style: dashboard

--- src/components/Dashboard/components/TaskProgress/TaskProgress.tsx
+++ src/components/Dashboard/components/TaskProgress/TaskProgress.tsx
@@ -18,8 +18,8 @@ export default function TaskProgressChart({ tasks, timeFilter }: DashboardChildP
         <DashboardItem title="Task Progress" hasContainer>
             <ResponsiveContainer>
                 <AreaChart data={data}>
-                    <XAxis dataKey="date" />
-                    <YAxis />
+                    <XAxis dataKey="date" fontSize={14} />
+                    <YAxis fontSize={14} />
                     <Tooltip />
                     {TASK_PROGRESS_LEGEND_DATA.map(({ key, color }) => (
                         <Area key={key} type="monotone" dataKey={key} stackId="1" stroke={color} fill={color} />
