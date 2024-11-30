0949514404dec34b89d13659d02bc5460fdebe33
omer <omerme13@gmail.com>
2024-11-30 20:10:38 +0200

    refactor: style settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
-import svgr from 'vite-plugin-svgr';
+import path from "path";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	plugins: [react(), svgr()],
+    plugins: [react(), svgr()],
     css: {
         preprocessorOptions: {
             scss: {
-                api: "modern-compiler",
+                additionalData:`
+					@import "./src/styles/_breakpoints.scss"; 
+                    @import "./src/styles/_utilities.scss"; 
+                    @import "./src/styles/_reset.scss"; 
+                    @import "./src/styles/_variables.scss";
+				`,
             },
         },
     },
